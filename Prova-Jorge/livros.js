7b4bd37ba8c882f13a5ab0d3607f959356ba2262
lucaaroeiracrv <143573759+lucaaroeiracrv@users.noreply.github.com>
2024-03-12 13:21:43 -0300

    Atualizar o livros.js

--- Prova-Jorge/livros.js
+++ Prova-Jorge/livros.js
@@ -29,25 +29,10 @@ let alunos = [
 ]
 
 
-    function criarPilha(){
-    return new pilha[0];
-    }
+    function criarPilha()
     
-    function inserirLivro(pilha,livro){
-        for (let i=0; i < alunos.length; i++) {
-        pilha.add(livro[i]);
-        console.log(`Livro ${alunos.titulo} esta na pilha.`);
-        }
-    }
+    function inserirLivro(pilha,livro)  
 
-    function tirarLivro(pilha){
-        for (let i=0; i < alunos.length; i++) {
-            const livroTirado = pilha.pop();
-            console.log(`Livro ${alunos.titulo} foi removido`);
-        }
-    }
-
-    function verificarLivro(pilha, titulo) {
-        const livroNaPilha = pilha.
-        
-    }
+    function tirarLivro(pilha)
+      
+    function verificarLivro(pilha, titulo)

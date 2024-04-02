8d9dc930ed1d5c580a7f8ad8cf330cd943043ef3
lucaaroeiracrv <143573759+lucaaroeiracrv@users.noreply.github.com>
2024-04-02 11:52:04 -0300

    Add files via upload

---
+++ acho q ta certo/teste/funcoes.js
@@ -0,0 +1,37 @@
+
+export function criarFila(tamanho=2) {
+    return [...new Array(tamanho)]
+}
+
+    export function colocarNaFila(fila = [], item) {
+        if (fila.includes(undefined)) {
+            fila[fila.indexOf(undefined)] = item
+            return
+        }
+        console.log("fila cheia");
+        return
+    }
+export function retirarDaFila(fila = []) {
+    if (fila[0] === undefined) {
+        console.log("fila vazia");
+        return
+    }
+    let primeiroLugar = fila[0]
+    for (let i = 0; i < fila.length -1; i++) {
+        fila[i] = fila[i+1]
+    }
+    fila[fila.length -1] = undefined
+    console.log(primeiroLugar);
+    return primeiroLugar
+}
+export function limparFila(fila=[]) {
+        if (fila[0] === undefined) {
+            console.log("a fila ja esta vazia");
+            return
+        }
+        let primeiro = fila[0]
+        for (let i = 0; i < fila.length; i++) {
+            fila[i]=undefined
+    }
+    fila[0]= primeiro
+}

4e0b9c4574d437eef3d22677afe91e55b7c8f529
lucaaroeiracrv <143573759+lucaaroeiracrv@users.noreply.github.com>
2024-04-02 12:44:18 -0300

    Add files via upload

---
+++ opa/funcoes.js
@@ -0,0 +1,49 @@
+export function criarFIla(tamanho=2) {
+    return [...new Array(tamanho)]
+}
+export function inserirFila(fila=[], item) {
+    if (fila.includes(undefined)) {
+        fila[fila.indexOf(undefined)] = item
+        return
+    }
+    console.log("a fila ja esta cheia");
+}
+
+
+
+export function removerFila(fila=[]) {
+    if (fila[0] === undefined) {
+        console.log("A fila esta vazia");
+    }
+    let primeiro = fila[0]
+    for (let i = 0; i < fila.length; i++) {
+        fila[i] = undefined   
+        }
+        fila[0]= primeiro
+    }
+    
+
+
+
+
+
+export function verTamanhofila(fila=[]){
+    
+    console.log(fila)
+    for (let i = 0; i < fila.length; i++) {
+        if (fila[i]===undefined) {
+            let emptySpace = empty_space +1
+        }
+        let usedSpace = usedSpace +1
+        let temCosia= fila[i]
+        let size = fila.length
+    }
+let obejto[{
+    "empty_space": emptySpace,
+    "used_space": usedSpace,
+    "size": size,
+    "sub_queue": fila(usedSpace)
+}];
+
+console.log(obejto);
+}
---
+++ opa/index.js
@@ -0,0 +1,5 @@
+import {  } from "";
+import { criarFIla, inserirFila, removerFila } from "./funcoes";
+criarFIla(fila)
+inserirFila(fila, primeiro)
+removerFila(fila)
